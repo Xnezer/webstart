eb7cfe921897ac22b918d4123beff52f8d55c681
Xnezer <yakowebdev@yandex.ru>
2020-03-17 19:36:05 +0300

    feat: added closing rules for modal and rewrite js code on jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event){
+/*document.addEventListener("DOMContentLoaded", function(event){
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close')
@@ -11,4 +11,33 @@ document.addEventListener("DOMContentLoaded", function(event){
   });
 
   closeBtn.addEventListener('click', switchModal);
+});
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.addClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.removeClass('modal--visible');
+  });
+
+  $(document).mouseup(function (e){
+    var div = $(".modal__dialog");
+    if (!div.is(e.target)
+        && div.has(e.target).length === 0) {
+      modal.removeClass('modal--visible');
+    };
+  }); 
+
+  document.addEventListener('keyup', function (e) {
+    if (e.key === 'Escape') {
+      modal.removeClass('modal--visible');
+    };
+  })
+
 });
