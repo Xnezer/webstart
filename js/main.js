4945e8b56d22efd8c22192a7122db89d77592d04
Xnezer <yakowebdev@yandex.ru>
2020-03-21 18:53:10 +0300

    feat: added form validate

--- js/main.js
+++ js/main.js
@@ -45,4 +45,40 @@ $(document).ready(function () {
 
   new WOW().init();
   
+  //Валидация формы
+  $('.modal__form').validate({
+
+    wrapper: "span", debug:true,
+    errorElement: "div",
+    errorClass: "invalid",
+    rules: {
+      // строчное правило
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+      },
+      userPhone: "required",
+      // правило - объект (блок)
+      userEmail: {
+        required: true,
+        email: true
+      }
+    }, // сообщение
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя даолжно быть не короче 2 символов",
+        maxlength: "Имя даолжно быть не длинее 15 символов"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Обязательно укажите email",
+        email: "Введите в формате name@domain.com"
+      }
+    }
+  })
+
+  // маска для телефона
+  $('[type=tel]').mask('+7-(000)-000-00-00', {placeholder: "+7-(___)-___-__-__"});
 });
